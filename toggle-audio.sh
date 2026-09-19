e5e3af151287490b210975c0a09e1dc9dbf2a104
Oleg Lodygensky <oleg.lodygensky@proton.me>
2026-09-19 10:37:57 +0200

    the applescript application do not have /opt/homebrew/bin:/opt/homebrew/opt/openssl@3/bin:/opt/homebrew/opt/openjdk/bin:/opt/homebrew/Cellar/openssl@3/3.6.0/bin/:/Users/mboleg/.nvm/versions/node/v22.22.3/bin:/opt/homebrew/opt/go/libexec/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/pkg/env/global/bin:/opt/X11/bin:/Library/Apple/usr/bin:/Library/TeX/texbin:/opt/homebrew/bin:/opt/homebrew/sbin:/Users/mboleg/go/bin:-tools:/users/mboleg/sbt/bin:/Users/mboleg/.nargo/bin:/Users/mboleg/.bb, so we must hardcode the call to SwitchAudioSource

--- toggle-audio.sh
+++ toggle-audio.sh
@@ -6,12 +6,12 @@ SWITCH="/opt/homebrew/bin/SwitchAudioSource"
 CURRENT=$("$SWITCH" -c -t output)
 
 if [[ "$CURRENT" == "BlackHole 2ch" ]]; then
-    SwitchAudioSource -s "MacBook Air Microphone" -t input
-    SwitchAudioSource -s "MacBook Air Speakers" -t output
+    "$SWITCH" -s "MacBook Air Microphone" -t input
+    "$SWITCH" -s "MacBook Air Speakers" -t output
     echo "🎤 🔊 Mac"
 else
-    SwitchAudioSource -s "BlackHole 2ch" -t input
-    SwitchAudioSource -s "BlackHole 2ch" -t output
+    "$SWITCH" -s "BlackHole 2ch" -t input
+    "$SWITCH" -s "BlackHole 2ch" -t output
     echo "⬛ BlackHole 2ch"
 fi
 
